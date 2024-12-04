44e6b9f810f7a6161c51c832841d97bf91d083d9
Asfand Ali <96019915+asfandalidal@users.noreply.github.com>
2024-12-04 18:48:11 +0500

    Update WeatherRepositoryImpl.kt

--- app/src/main/java/com/example/myweather/main/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/myweather/main/WeatherRepositoryImpl.kt
@@ -5,6 +5,6 @@ import com.example.myweather.network.ApiService
 
 class WeatherRepositoryImpl(private val apiService: ApiService) : WeatherRepository {
     override suspend fun getCityData(cityName: String): WeatherResponse {
-        return apiService.getWeatherForecast(cityName, "f8a7cf4e052a8672bb9cbc617057dff7")
+        return apiService.getWeatherForecast(cityName, api_key)
     }
 }
